5e66fac43e2cc4cbb85a90ca93e950bc48ffcb22
Darren Kwong <darrenkwong@Darrens-MacBook-Pro.local>
2022-11-08 15:12:17 -0800

    refactor code moving all into async function, and adding google-cloud/secret-manager

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ Slack app built with https://slack.dev/bolt-js/reference . This app can post mes
 Get .env keys from your slack app and add them to .env file, see .env.example
 
 Run app with command 'node app.js' or 'npm run start'
+
+todo:
+[ ] validate user is in admin channel to submit slash command
+[ ] update app icon
+[ ] update modal messaging
--- app.js
+++ app.js
@@ -1,254 +1,270 @@
-require('dotenv').config();
 const { App } = require('@slack/bolt');
+require('dotenv').config();
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
 
-// Initializes your app with your bot token and signing secret
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true, 
-  appToken: process.env.SLACK_APP_TOKEN,
-  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
-  // you still need to listen on some port!
-  // port: process.env.PORT || 3000
-});
+async function accessSecretVersion (name) {
+  // const client = new SecretManagerServiceClient()
+  // const projectId = process.env.PROJECT_ID
+  // const [version] = await client.accessSecretVersion({
+  //   name: `projects/${projectId}/secrets/${name}/versions/1`
+  // })
+
+  // // Extract the payload as a string.
+  // const payload = version.payload.data.toString('utf8')
+
+  // return payload
+  return name
+}
 
 (async () => {
+
+  // Initializes your app with your bot token and signing secret
+  const app = new App({
+    token: process.env.SLACK_BOT_TOKEN || await accessSecretVersion('slack-bot-token'),
+    signingSecret: process.env.SLACK_SIGNING_SECRET || await accessSecretVersion('slack-signing-secret'),
+    socketMode: true, 
+    appToken: process.env.SLACK_APP_TOKEN || await accessSecretVersion('slack-app-token'),
+    // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+    // you still need to listen on some port!
+    // port: process.env.PORT || 3000
+  });
+
   // Start your app
   await app.start();
   
   console.log('⚡️ Bolt app is running!');
 
-})();
+  
+  //create Home tab view
+  app.event('app_home_opened', async ({ event, client, context }) => {
+    console.log(event);
+    try {
+      /* view.publish is the method that your app uses to push a view to the Home tab */
+      const result = await client.views.publish({
 
-//create Home tab view
-app.event('app_home_opened', async ({ event, client, context }) => {
-  console.log(event);
-  try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
+        /* the user that opened your app's app home */
+        user_id: event.user,
 
-      /* the user that opened your app's app home */
-      user_id: event.user,
+        /* the view object that appears in the app home*/
+        view: {
+          type: 'home',
+          callback_id: 'home_view',
 
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_, see list of commands below* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Available commands:*\n*/postallchannels:* post a message to all channels this bot is a member of."
-            }
-          }
-        ]
-      }
-    });
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
-
-// Listen for a slash command invocation
-app.command('/postallchannels', async ({ ack, body, client, logger }) => {
-  // Acknowledge the command request
-  await ack();
-
-  // check if userid exists in admin channel, if not, decline request
-  const user = body.user_id;
-  console.log('userid is ', body.user_id)
-
-  const adminList = await getAdminMembers();
-
-  if (adminList.includes(user)) {
-    console.log("cool, you're an admin")
-  }
-  else{
-    console.log("sorry, you're not a member")
-    return;
-  }
-
-
-  try {
-    // Call views.open with the built-in client
-    const result = await client.views.open({
-      // Pass a valid trigger_id within 3 seconds of receiving it
-      trigger_id: body.trigger_id,
-      // View payload
-      view: {
-        type: 'modal',
-        // View identifier
-        callback_id: 'post_all_channels',
-        title: {
-          type: 'plain_text',
-          text: 'Post to all channels'
-        },
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Submitting this modal will post your message to all channels the bot is a member of.'
-            }
-          },
-          {
-            type: 'input',
-            block_id: 'block_1',
-            label: {
-              type: 'plain_text',
-              text: 'What is your message?'
+          /* body of the view */
+          blocks: [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome to your _App's Home_, see list of commands below* :tada:"
+              }
             },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'post_message',
-              multiline: true
+            {
+              "type": "divider"
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Available commands:*\n*/postallchannels:* post a message to all channels this bot is a member of."
+              }
             }
-          }
-        ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
+          ]
         }
-      }
-    });
-    logger.info(result);
-  }
-  catch (error) {
-    logger.error(error);
-  }
-});
+      });
+    }
+    catch (error) {
+      console.error(error);
+    }
+  });
 
-// Handle view_submission request for when someone submits modal 'post_all_channels'
-app.view('post_all_channels', async ({ ack, body, view, client, logger }) => {
-  // Acknowledge the view_submission request
-  await ack();
+  // Listen for a slash command invocation
+  app.command('/postallchannels', async ({ ack, body, client, logger }) => {
+    // Acknowledge the command request
+    await ack();
 
-  // Do whatever you want with the input data
-  
-  // Assume there's an input block with `block_1` as the block_id and `post_message` action_id
-  const val = view['state']['values']['block_1']['post_message'];
-  const user = body['user']['id'];
+    // check if userid exists in admin channel, if not, decline request
+    const user = body.user_id;
+    console.log('userid is ', body.user_id)
 
-  // if user isn't in admin channel then decline submission
-  // console.log('userid', user)
+    const adminList = await getAdminMembers();
 
-  postMessageToChannels(val.value);
+    if (adminList.includes(user)) {
+      console.log("cool, you're an admin")
+    }
+    else{
+      console.log("sorry, you're not a member")
+      return;
+    }
 
-  // Message to send user
-  let msg = '';
+    try {
+      // Call views.open with the built-in client
+      const result = await client.views.open({
+        // Pass a valid trigger_id within 3 seconds of receiving it
+        trigger_id: body.trigger_id,
+        // View payload
+        view: {
+          type: 'modal',
+          // View identifier
+          callback_id: 'post_all_channels',
+          title: {
+            type: 'plain_text',
+            text: 'Post to all channels'
+          },
+          blocks: [
+            {
+              type: 'section',
+              text: {
+                type: 'mrkdwn',
+                text: 'Submitting this modal will post your message to all channels the bot is a member of.'
+              }
+            },
+            {
+              type: 'input',
+              block_id: 'block_1',
+              label: {
+                type: 'plain_text',
+                text: 'What is your message?'
+              },
+              element: {
+                type: 'plain_text_input',
+                action_id: 'post_message',
+                multiline: true
+              }
+            }
+          ],
+          submit: {
+            type: 'plain_text',
+            text: 'Submit'
+          }
+        }
+      });
+      logger.info(result);
+    }
+    catch (error) {
+      logger.error(error);
+    }
+  });
 
-  msg = `I posted your message: ${val.value}`;
-  // Message the user
-  try {
-    await client.chat.postMessage({
-      channel: user,
-      text: msg
-    });
-  }
-  catch (error) {
-    logger.error(error);
-  }
+  // Handle view_submission request for when someone submits modal 'post_all_channels'
+  app.view('post_all_channels', async ({ ack, body, view, client, logger }) => {
+    // Acknowledge the view_submission request
+    await ack();
 
-});
-
-// Find conversation ID using the users.conversations method
-async function findChannels() {
-
-  const channelIds = [];
-  
-  try {
-    // Call the conversations.list method using the built-in WebClient
-    const result = await app.client.users.conversations({
-      // The token you used to initialize your app
-      token: process.env.SLACK_BOT_TOKEN
-    });
+    // Do whatever you want with the input data
     
-    //generate list of channelIds
-    for (const channel of result.channels) {
-      channelIds.push(channel.id);
+    // Assume there's an input block with `block_1` as the block_id and `post_message` action_id
+    const val = view['state']['values']['block_1']['post_message'];
+    const user = body['user']['id'];
+
+    // if user isn't in admin channel then decline submission
+    // console.log('userid', user)
+
+    postMessageToChannels(val.value);
+
+    // Message to send user
+    let msg = '';
+
+    msg = `I posted your message: ${val.value}`;
+    // Message the user
+    try {
+      await client.chat.postMessage({
+        channel: user,
+        text: msg
+      });
+    }
+    catch (error) {
+      logger.error(error);
+    }
+
+  });
+
+  // Find all channels the bot is a member of
+  async function findChannels() {
+
+    const channelIds = [];
+    
+    try {
+      // Call the conversations.list method using the built-in WebClient
+      const result = await app.client.users.conversations({
+        // The token you used to initialize your app
+        token: process.env.SLACK_BOT_TOKEN
+      });
+      
+      //generate list of channelIds
+      for (const channel of result.channels) {
+        channelIds.push(channel.id);
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+    
+    return channelIds;
+    
+  }
+    
+  // Post a message to a channel your app is in using ID and message text
+  async function publishMessage(id, text) {
+    try {
+      // Call the chat.postMessage method using the built-in WebClient
+      const result = await app.client.chat.postMessage({
+        // The token you used to initialize your app
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: id,
+        text: text,
+        // You could also use a blocks[] array to send richer content
+      });
+
+      // Print result, which includes information about the message (like TS)
+      // console.log(result);
+    }
+    catch (error) {
+      console.error(error);
     }
   }
-  catch (error) {
-    console.error(error);
+
+  async function getAdminMembers() {
+    try {
+      // Call the conversations.list method using the WebClient
+      const result = await app.client.conversations.list({
+        types: "private_channel, public_channel",
+      });
+
+      const channels = result.channels;
+
+      //get adminChannel object
+      const adminChannel = channels.find( el => el.name === 'darren_admins')
+      console.log('foundobj', adminChannel);
+
+      //get all member ids in adminChannel
+      const adminMembers = await app.client.conversations.members({
+        channel: adminChannel.id,
+      })
+
+      const adminMemberList = adminMembers.members;
+      // console.log('adminMemberListtttt', adminMemberList)
+      
+      return adminMemberList;
+
+    }
+    catch (error) {
+      console.error(error);
+    }
   }
-  
-  return channelIds;
-  
-}
-  
-// Post a message to a channel your app is in using ID and message text
-async function publishMessage(id, text) {
-  try {
-    // Call the chat.postMessage method using the built-in WebClient
-    const result = await app.client.chat.postMessage({
-      // The token you used to initialize your app
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: id,
-      text: text,
-      // You could also use a blocks[] array to send richer content
-    });
 
-    // Print result, which includes information about the message (like TS)
-    // console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
-}
+  getAdminMembers();
 
-async function getAdminMembers() {
-  try {
-    // Call the conversations.list method using the WebClient
-    const result = await app.client.conversations.list({
-      types: "private_channel, public_channel",
-    });
-
-    const channels = result.channels;
-
-    //get adminChannel object
-    const adminChannel = channels.find( el => el.name === 'darren_admins')
-    console.log('foundobj', adminChannel);
-
-    //get all member ids in adminChannel
-    const adminMembers = await app.client.conversations.members({
-      channel: adminChannel.id,
+  // Get channels bot is member of, then send a message to all channels
+  async function postMessageToChannels(message) {
+    (async () => {
+      return await findChannels()
+    })().then(channelIds => {
+      console.log('all channelIds', channelIds)
+      channelIds.map(id => {
+        publishMessage(id, message);
+      })
     })
-
-    const adminMemberList = adminMembers.members;
-    // console.log('adminMemberListtttt', adminMemberList)
-    
-    return adminMemberList;
-
   }
-  catch (error) {
-    console.error(error);
-  }
-}
 
-getAdminMembers();
-
-// Get channels bot is member of, then send a message to all channels
-async function postMessageToChannels(message) {
-  (async () => {
-    return await findChannels()
-  })().then(channelIds => {
-    console.log('all channelIds', channelIds)
-    channelIds.map(id => {
-      publishMessage(id, message);
-    })
-  })
-}
+})();
